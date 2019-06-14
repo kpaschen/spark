2d308f9432c4cd3926e03b220c9a3abe30ae9d54
Kathrin Paschen <kathrin.paschen@gmail.com>
2019-06-14 09:35:13 +0200

    Add option to read from parquet. Also changed order of output and input files to be consistent with python version.

--- dblp/scala/src/main/scala/com/nephometrics/dblp/CitationsRunner.scala
+++ dblp/scala/src/main/scala/com/nephometrics/dblp/CitationsRunner.scala
@@ -2,41 +2,62 @@
 
 package com.nephometrics.dblp
 
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
 /**
  * Example Usage:
  * {{{
- * sbt run CitationsRunner <path to input files> <path to output>
+ * sbt run CitationsRunner <path to output> <input files>
+ * If you have wildcards in the input files, enclose them in quotes
+ * to avoid the shell expanding the wildcards.
  * }}}
  */
 object CitationsRunner {
   def main(args: Array[String]) {
     if (args.length < 2) {
-      System.err.println("Usage: CitationsRunner <input files> <output path>")
+      System.err.println("Usage: CitationsRunner <output path> <input files>")
       System.exit(1)
     }
 
-    val filename = args(0)
-    val outputs = args(1)
+    val filename = args(1)
+    val outputs = args(0)
+
+    System.err.println("outputs to to " + outputs)
+    System.err.println("inputs: " + filename)
 
     val spark = SparkSession
       .builder
       .appName("citations")
       .getOrCreate()
 
-    // spark.sparkContext.setLogLevel(org.apache.log4j.Level.ERROR.toString())
+    //spark.sparkContext.setLogLevel(org.apache.log4j.Level.ERROR.toString())
     val stats = new Citations(spark)
 
-    // This selects a subset of fields and specifies types and nullability,
-    // which is useful because I know the id field is not null.
-    // Also ensures year is an integer not a long.
-    val schema = StructType(Array(
-      StructField("id", StringType, false),
-      StructField("references", ArrayType(StringType, false), true),
-      StructField("year", IntegerType, false)))
-    val baseData = spark.read.schema(schema).json(filename)
+    // You can't initialize this using _ because the JVM doesn't know
+    // how to create a default instance. The null.asInstanceOf[T] pattern
+    // is apparently the recommended option.
+    var baseData:DataFrame = null.asInstanceOf[DataFrame]
+
+    // See if we're loading json or parquet data.
+    if (filename.endsWith("json")) {
+      // This selects a subset of fields and specifies types and nullability,
+      // which is useful because I know the id field is not null.
+      // Also ensures year is an integer not a long.
+      val schema = StructType(Array(
+        StructField("id", StringType, false),
+        StructField("references", ArrayType(StringType, false), true),
+        StructField("year", IntegerType, false)))
+        baseData = spark.read.schema(schema).json(filename)
+    } else {
+      // You can't use a schema to read a parquet file when that requires
+      // typecasting (UnsupportedOperationException), so read it like this,
+      // the later 'select' statements will be clever enough to only read
+      // what they need.
+      val tmp = spark.read.parquet(filename)
+      baseData = tmp.withColumn("year", tmp("year").cast(IntegerType))
+    }
     val idRefs = stats.idRefYearDs(baseData)
     val counted = stats.countCitationsByYear(idRefs)
     val cited = stats.countCitationsByAge(counted, baseData)
